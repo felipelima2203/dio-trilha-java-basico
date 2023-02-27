962e68fa702d05e32105a9571bd48bb30d14fc66
felipelima2203 <felipelima2203@gmail.com>
2023-02-27 00:53:04 -0300

    operadores

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        final int numero =10;
+        int numero2 = 5;
+        numero2 = -numero2;
+        //numero=numero*numero*2;
+        System.out.println(numero);
+        System.out.println(numero2);
+        numero2*= -1;
+        System.out.println(numero2);
     }
 }
